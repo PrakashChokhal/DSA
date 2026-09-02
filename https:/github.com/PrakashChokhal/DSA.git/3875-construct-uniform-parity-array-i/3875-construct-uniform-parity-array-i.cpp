d509f471e03031b29819b57fe9f951f7749d8b64
PrakashChokhal <prakashchokhal01@gmail.com>
2026-09-02 21:32:28 +0530

    Time: 0 ms (100%), Space: 30.1 MB (94.95%) - LeetHub-Neo

---
+++ https:/github.com/PrakashChokhal/DSA.git/3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        
+        return true;
+        // Since all elements in nums1 are distinct, it is always possible to make all elements in nums2 odd, so the function should always return true.
+    }
+};
